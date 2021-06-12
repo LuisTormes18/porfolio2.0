aa262edc6846f18c19f9d5c9ea311609c1db85d3
Luis Tormes <luisangeltormes@gmail.coom>
2021-06-12 08:37:03 -0400

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+// año actual
+// const $yearNow = document.querySelector("#yearNow");
+// let year = new Date();
+// $yearNow.innerHTML = year.getFullYear();
+
+const toogleMenu = () => {
+  const $nav = document.getElementById("navbar");
+  $nav.classList.toggle("active-menu-mobile");
+};
+
+const $btnMobile = document.getElementById("btn-mobile");
+$btnMobile.addEventListener("click", toogleMenu);
+// $btnMobile.addEventListener("touchstart", toogleMenu);
+
+//   const $header = document.querySelector(".header");
+  //   $header.classList.toggle("active");
+
+  // bacground header scroll
+
+  const $header = document.querySelector(".header");
+  const $session1 = document.querySelector("#session-1");
+
+  const options = {};
+
+const observer = new IntersectionObserver(
+function(entries,observer)
+{ 
+	entries.forEach(entry =>{
+		if(!entry.isIntersecting){$header.classList.add('active');}
+		else{$header.classList.remove('active')}
+	})
+},
+options)
+
+observer.observe($session1);
