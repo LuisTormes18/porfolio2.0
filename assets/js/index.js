4ca2d4be8ad74d45ed0483ab070c7c771e151a92
Luis Tormes <luisangeltormes@gmail.coom>
2021-07-04 14:42:26 -0400

    modificando el sticki navbar y algunos estilos del header

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,3 @@
-// año actual
-// const $yearNow = document.querySelector("#yearNow");
-// let year = new Date();
-// $yearNow.innerHTML = year.getFullYear();
 
 const toogleMenu = () => {
   const $nav = document.getElementById("navbar");
@@ -12,26 +8,16 @@ const $btnMobile = document.getElementById("btn-mobile");
 $btnMobile.addEventListener("click", toogleMenu);
 
 
-// $btnMobile.addEventListener("touchstart", toogleMenu);
+const $header = document.querySelector(".header");
+const $session1 = document.querySelector("#session-1");
 
-//   const $header = document.querySelector(".header");
-  //   $header.classList.toggle("active");
+window.onscroll = ()=>{
 
-  // bacground header scroll
-
-  const $header = document.querySelector(".header");
-  const $session1 = document.querySelector("#session-1");
-
-  const options = {};
-
-const observer = new IntersectionObserver(
-function(entries,observer)
-{ 
-	entries.forEach(entry =>{
-		if(!entry.isIntersecting){$header.classList.add('active');}
-		else{$header.classList.remove('active')}
-	})
-},
-options)
-
-observer.observe($session1);
+  let Scrollposition = window.pageYOffset;
+  
+  if (Scrollposition > 0) {
+    $header.classList.add('active');
+  } else {
+    $header.classList.remove('active');
+  }
+}
