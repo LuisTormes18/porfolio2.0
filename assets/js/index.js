ccaa00478248c9d227e759f4d45bcc5c284e8f20
Luis Tormes <luisangeltormes@gmail.coom>
2021-07-09 11:29:04 -0400

    arregalagando errores, cambiando el logo de un texto a una imagen y poniendo el target=_blank a los enlaces

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,6 +10,7 @@ $btnMobile.addEventListener("click", toogleMenu);
 
 const $header = document.querySelector(".header");
 const $session1 = document.querySelector("#session-1");
+const $logo = document.getElementById('img-logo');
 
 window.onscroll = ()=>{
 
@@ -17,7 +18,10 @@ window.onscroll = ()=>{
   
   if (Scrollposition > 0) {
     $header.classList.add('sticky');
+    $logo.setAttribute('src','./assets/img/logo_claro.png')
   } else {
     $header.classList.remove('sticky');
+    $logo.setAttribute('src','./assets/img/logo.png')
+
   }
 }
