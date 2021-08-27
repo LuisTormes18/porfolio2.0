82aa4d10d90874de19c9466e519ef7f78113a4ea
Luis Tormes <luisangeltormes@gmail.coom>
2021-08-27 13:14:12 -0400

    arreglando las imagenes de los proyectos

--- assets/js/index.js
+++ assets/js/index.js
@@ -29,6 +29,58 @@ window.onscroll = ()=>{
 // load projects
 const projects = [];
 
-const loadProjects = () =>{
+const paintProjects = (baseTag, projects, numCards) =>{
+
+    for(const element in projects){
+
+      const $div_card = createElement('div');
+      $div_card.setAttribute('class','card');
+
+      const $card_img = createElement('div');
+      $card_img.innerHTML = `<img src="${projects.src}" alt="" />`
+
+
+      const $card_title = createElement('div');
+      $card_title.innerHTML = `<h2>${projects.title}</h2>`
+
+       const $card_skills = createElement('ul');
+      
+      for(const skill in projects.skills){
+
+       const $li = createElement('li');
+
+      $li.innerHTML = `<img src="${skill.img}" alt="" />`
+
+      $card_skills.appendChild($li);
+      
+      }
+
+
+      const $card_btn = createElement('div');
+
+$card_btn.innerHTML =` <a
+                href="${projects.url}"
+                target="_blank"
+                rel="noreferrer"
+                >see more.</a
+              >`;
+
+      const $card_footer = createElement('div');
+    $card_footer.appendChild($card_title);
+    $card_footer.appendChild($card_title);
+    $card_footer.appendChild($card_btn);
+
+    $div_card.appenChild($card_img);
+    $div_card.appenChild($card_footer);
+
+    }
+
+
+
+
 
 }
+
+const $cardsPageOthres = ddocument.getElementById('cardsPageOthres');
+const $indexCards = ddocument.getElementById('indexCards');
+
